62099048d3aaa0e897dae234f6392927e19f4447
Haydn Ewers <27211+haydn@users.noreply.github.com>
2025-03-30 12:45:37 +1100

    add layout to docs app

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -1,17 +1,45 @@
+import "@colonydb/anthill/foundation/Global";
 import type { Metadata } from "next";
+import Link from "next/link";
+import type { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
 
 export const metadata: Metadata = {
   title: "Anthill | Documentation",
   description: "Reference documentation for Anthill, ColonyDB's design system.",
 };
 
-const RootLayout = ({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) => (
+const RootLayout = ({ children }: Props) => (
   <html lang="en">
-    <body>{children}</body>
+    <body>
+      <div
+        style={{
+          borderRight: "1px solid #ccc",
+          height: "100dvh",
+          overflowY: "auto",
+          padding: "1lh",
+          position: "fixed",
+          width: "24ch",
+        }}
+      >
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+        </ul>
+      </div>
+      <div
+        style={{
+          marginLeft: "24ch",
+          padding: "1lh",
+        }}
+      >
+        {children}
+      </div>
+    </body>
   </html>
 );
 
