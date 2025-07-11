c72ba0b9c96f0a9320589aabc8298857108e3079
Haydn Ewers <27211+haydn@users.noreply.github.com>
2025-07-11 16:04:12 +1000

    improve spacing and heading level handling in <Card>

--- packages/anthill/src/Card.tsx
+++ packages/anthill/src/Card.tsx
@@ -1,32 +1,40 @@
 "use client";
 
 import clsx from "clsx";
-import type { JSX, ReactNode } from "react";
+import { type JSX, type ReactNode, useContext } from "react";
 import styles from "./Card.module.css";
+import { HeadingLevelContext } from "./HeadingLevelContext.js";
 import { Section } from "./Section.js";
+import { Stack } from "./Stack.js";
 
 type Props = {
   children: ReactNode;
   footer?: ReactNode;
   header?: ReactNode;
+  headingLevel?: 1 | 2 | 3 | 4 | 5 | 6;
   icon?: ReactNode;
   id?: string;
   tagName?: keyof JSX.IntrinsicElements;
 };
 
-export const Card = ({ children, footer, header, icon, id, tagName: Tag = "section" }: Props) => (
-  <Tag className={clsx(styles.container, icon ? styles.withIcon : undefined)} id={id}>
-    {icon ? <div className={styles.icon}>{icon}</div> : null}
-    <div className={styles.content}>
-      {header ? <div className={styles.header}>{header}</div> : null}
-      <Section spacing="p1" tagName="div">
-        {children}
-      </Section>
-      {footer ? (
-        <Section spacing="p1" tagName="div">
-          <div className={styles.footer}>{footer}</div>
-        </Section>
-      ) : null}
-    </div>
-  </Tag>
-);
+export const Card = ({ children, footer, header, headingLevel, icon, id, tagName: Tag = "section" }: Props) => {
+  const contextHeadingLevel = useContext(HeadingLevelContext);
+  return (
+    <HeadingLevelContext.Provider value={headingLevel ?? contextHeadingLevel}>
+      <Tag className={clsx(styles.container, icon ? styles.withIcon : undefined)} id={id}>
+        {icon ? <div className={styles.icon}>{icon}</div> : null}
+        <div className={styles.content}>
+          {header ? <div className={styles.header}>{header}</div> : null}
+          <Stack spacing="p1" tagName="div">
+            {children}
+          </Stack>
+          {footer ? (
+            <Section spacing="p1" tagName="div">
+              <div className={styles.footer}>{footer}</div>
+            </Section>
+          ) : null}
+        </div>
+      </Tag>
+    </HeadingLevelContext.Provider>
+  );
+};
