bf1e7c25a86416c437bd1071a4e95bcaf7ed0eba
Haydn Ewers <27211+haydn@users.noreply.github.com>
2025-07-14 09:59:49 +1000

    update the icons used in <CopyToClipboardAction>

--- packages/anthill/src/CopyToClipboardAction.tsx
+++ packages/anthill/src/CopyToClipboardAction.tsx
@@ -13,7 +13,7 @@ export const CopyToClipboardAction = ({ text, ...props }: Props) => {
   const state = currentTimeout ? "copied" : "default";
   return (
     <Action
-      icon={state === "default" ? <Icon symbol="Remove" /> : <Icon color="green-t2" symbol="Pin" />}
+      icon={state === "default" ? <Icon symbol="Copy" /> : <Icon color={["green-s2", "green-t2"]} symbol="Tick" />}
       onClick={async () => {
         await navigator.clipboard.writeText(text);
         if (currentTimeout) clearTimeout(currentTimeout);
