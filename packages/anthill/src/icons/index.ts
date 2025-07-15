8646484875c9d9a6793d7809e94a6aaefb1c5756
Haydn Ewers <27211+haydn@users.noreply.github.com>
2025-07-16 09:43:40 +1000

    add placeholder "Add" icon symbol

--- packages/anthill/src/icons/index.ts
+++ packages/anthill/src/icons/index.ts
@@ -2,7 +2,13 @@
 // of the `Icon` component. An icon can be exported multiple times with
 // different names if needed.
 export * from "./External.js";
-export { External as Copy, External as Processing, External as Tick, External as Warning } from "./External.js";
+export {
+  External as Add,
+  External as Copy,
+  External as Processing,
+  External as Tick,
+  External as Warning,
+} from "./External.js";
 export * from "./GanttChart.js";
 export * from "./GanttChartItem.js";
 export * from "./List.js";
